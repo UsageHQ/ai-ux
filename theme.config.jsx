14d6a86232de5c8940e346e81b2a05f013102266
theapril-studio <thanhvuttv@gmail.com>
2024-03-05 15:26:08 +0700

    add github link

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
   logo: <span>UI pattern library for Generative AI product</span>,
   project: {
-    link: 'https://github.com/shuding/nextra',
+    link: 'https://github.com/UsageHQ/ai-ux',
   },
   // ... other theme options
 };
