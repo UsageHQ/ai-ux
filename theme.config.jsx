89b8e8f6796195123a3359e8dec73cf7c2e007ec
theapril-studio <thanhvuttv@gmail.com>
2024-03-05 15:23:57 +0700

    init project

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>UI pattern library for Generative AI product</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra',
+  },
+  // ... other theme options
+};
