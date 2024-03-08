27375a4b7aa9548724f114332149606cea563b3c
theapril-studio <thanhvuttv@gmail.com>
2024-03-08 11:06:21 +0700

    more content, metadata, themeconfig

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,35 @@
 export default {
   logo: <span>UI pattern library for Generative AI product</span>,
   project: {
-    link: 'https://github.com/UsageHQ/ai-ux',
+    link: "https://github.com/UsageHQ/ai-ux",
   },
-  docsRepositoryBase: 'https://github.com/UsageHQ/ai-ux/tree/main',
+  docsRepositoryBase: "https://github.com/UsageHQ/ai-ux/tree/main",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Usage.so",
+      defaultTitle: "UI pattern library for Generative AI product - Usage.so",
+      description:
+        "A comprehensive guides for designing UX for AI-powered products",
+    };
+  },
+  banner: {
+    key: "2.0-release",
+    text: (
+      <a href="https://usage.so" target="_blank">
+        🎉 Usage.so beta is comming soon! Subscribe to get early access.
+      </a>
+    ),
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://usage.so" target="_blank">
+          Usage.so
+        </a>
+        .
+      </span>
+    ),
+  },
   // ... other theme options
 };
